7abb125e2c57b56d7a1d446c642935af74ecc846
Vivek Tp <Vivektvp1@gmail.com>
2023-03-05 18:33:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(int(os.environ.get["BOT_TOKEN"]))
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 👋 Hello, This Bot Help you To Delete Service Messages In Groups/Channels.just add Me To Your group Or Channel As Admin With Delete Messages Permission')
+    bot.reply_to(message,'👋 Hello, This Bot Help you To Delete Service Messages In Groups/Channels.just add Me To Your group Or Channel As Admin With Delete Messages Permission')
 
 @bot.message_handler(content_types=['service'])
 def delete_service_messages(message):
