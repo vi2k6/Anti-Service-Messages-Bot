7d73a2fa9406c7cef78e362b58823718584e396a
Vivek Tp <Vivektvp1@gmail.com>
2023-03-05 18:32:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,42 +1,14 @@
-import os
-import logging
-from pyrogram import Client, filters
+import telebot
+from telebot import types
 
-tgbot = Client(
-    "Delete-Service-Messages-Bot",
-    bot_token=os.environ["BOT_TOKEN"],
-    api_id=int(os.environ["API_ID"]),
-    api_hash=os.environ["API_HASH"],
-)
+bot = telebot.TeleBot(int(os.environ.get["BOT_TOKEN"]))
 
-btname = os.environ["BOT_USERNAME"]
-service_filter = filters.service
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 👋 Hello, This Bot Help you To Delete Service Messages In Groups/Channels.just add Me To Your group Or Channel As Admin With Delete Messages Permission')
 
+@bot.message_handler(content_types=['service'])
+def delete_service_messages(message):
+    bot.delete_message(message.chat.id, message.message_id)
 
-# function to send message
-async def send_message(client, chat_id):
-    await client.send_message(chat_id, "Hello, Thanks For Adding Me to group.\n\n Now Make me Admin With <b>Delete Messages</b> Permission")
-
-# function to handle bot added to a group
-@tgbot.on_message(filters.new_chat_members)
-async def welcome(client, message):
-    for member in message.new_chat_members:
-        if member.id == client.get_me().id:
-            await send_message(client, message.chat.id)
-
-@tgbot.on_message(filters.text)
-def start(client, message):
-    client.send_message(message.chat.id, "Hi, This is a anti service bot.Just Add Me To Your Group By [Clicking Here](http://t.me/BOT_USERNAME?startgroup=botstart) Then I Will delete all berfore and after service messages in the group.")
-
-@tgbot.on_message(service_filter)
-async def delete_service_message(client, message):
-    # Delete service message
-    await client.delete_messages(chat_id=message.chat.id, message_ids=message.message_id)
-
-print(
-    """
-Bot {BOT_USERNAME} Started!!!
-"""
-)
-
-tgbot.run()
+bot.polling()
